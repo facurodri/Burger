ea265365cf5877439113fe610e25abb457a30f08
Facundo Rodriguez <facurodri91@gmail.com>
2019-04-29 11:56:51 -0400

    finished files and paths

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double CheeseBurger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("McGangBang Burger", false);
